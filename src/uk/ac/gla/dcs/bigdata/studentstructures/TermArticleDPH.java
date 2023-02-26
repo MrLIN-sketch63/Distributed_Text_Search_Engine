f72697aac81da1deaa5d36f307093b3ba5bc0722
Ziyang Lin <2761137L@student.gla.ac.uk>
2023-02-26 21:34:37 +0800

    test

--- src/uk/ac/gla/dcs/bigdata/studentstructures/TermArticleDPH.java
+++ src/uk/ac/gla/dcs/bigdata/studentstructures/TermArticleDPH.java
@@ -5,33 +5,23 @@ import java.io.Serializable;
 import uk.ac.gla.dcs.bigdata.providedstructures.NewsArticle;
 
 public class TermArticleDPH implements Serializable{
-	
+
 	private static final long serialVersionUID = 7329797824926066989L;
-	
-	double DPHscore = 0;
+
+	double DPHscore;
 	String terms;
 	NewsArticle article;
-	
-	
-<<<<<<<< HEAD:src/uk/ac/gla/dcs/bigdata/studentstructures/DPHall.java
-	
-	
-	public DPHall(double DPHscore, String terms, NewsArticle article) {
-========
+
+
 	public  TermArticleDPH () {}
-	
-	
+
+
 	public  TermArticleDPH (double dPHscore, String terms, NewsArticle  article) {
->>>>>>>> 24275447b157583d4f11648b5ae45ef57cd5d195:src/uk/ac/gla/dcs/bigdata/studentstructures/TermArticleDPH.java
 		super();
-		this.DPHscore = DPHscore;
+		this.DPHscore = dPHscore;
 		this.terms = terms;
 		this.article = article;
 	}
-	
-	public DPHall() {}
-
-	
 	@Override
 	public String toString() {
 		return "DPHall [DPHscore=" + DPHscore + ", terms=" + terms + ", article=" + article + "]";
@@ -57,5 +47,5 @@ public class TermArticleDPH implements Serializable{
 		this.article = article;
 	}
 
-	
+
 }
